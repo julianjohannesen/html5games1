c010d824016e86c4689767d15799154f610b76a0
Julian Johannesen <julian.johannesen@gmail.com>
2021-05-05 17:50:35 -0400

    Added comments

--- ch01/02-guess/src/main.js
+++ ch01/02-guess/src/main.js
@@ -1,12 +1,21 @@
 const myGuess = Math.floor(Math.random() * 20) + 1;
+// Keep track of the number of guesses the player has made
 let guesses = 0;
+// The player's guess
 let guess;
 
+// The simplest loop possible. While the player's guess
+// doesn't equal the computer's number ...
 while (guess !== myGuess) {
+  // Prompt to get a guess from the player
   guess = parseInt(prompt("What number am I thinking of?"), 10);
+  // Increment the number of guesses
   guesses++;
+  // If the player's guess is too low, tell them to 
+  // go higher
   if (guess < myGuess) {
     alert("Higher.");
+  // else tell them to go lower
   } else if (guess > myGuess) {
     alert("Lower.");
   }
